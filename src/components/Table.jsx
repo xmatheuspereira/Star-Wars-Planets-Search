e4f521b0e1e56186661867090c312c126f4d3cfb
Matheus Pereira <matheusdimmy@hotmail.com>
2022-05-01 15:41:53 -0300

    Requisito 2 OK

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -8,10 +8,6 @@ const Table = () => {
     value: '',
   });
 
-  // filteredPlanets = (planets) => {
-
-  // };
-
   return (
     <div>
       <header>
@@ -41,23 +37,25 @@ const Table = () => {
           </tr>
         </thead>
         <tbody>
-          {planets.map((item, index) => (
-            <tr key={ index }>
-              <td>{item.name}</td>
-              <td>{item.rotation_period}</td>
-              <td>{item.orbital_period}</td>
-              <td>{item.diameter}</td>
-              <td>{item.climate}</td>
-              <td>{item.gravity}</td>
-              <td>{item.terrain}</td>
-              <td>{item.surface_water}</td>
-              <td>{item.population}</td>
-              <td>{item.films}</td>
-              <td>{item.created}</td>
-              <td>{item.edited}</td>
-              <td>{item.url}</td>
-            </tr>
-          ))}
+          {planets
+            .filter(({ name }) => name.toLowerCase().includes(selected.value))
+            .map((item, index) => (
+              <tr key={ index }>
+                <td>{item.name}</td>
+                <td>{item.rotation_period}</td>
+                <td>{item.orbital_period}</td>
+                <td>{item.diameter}</td>
+                <td>{item.climate}</td>
+                <td>{item.gravity}</td>
+                <td>{item.terrain}</td>
+                <td>{item.surface_water}</td>
+                <td>{item.population}</td>
+                <td>{item.films}</td>
+                <td>{item.created}</td>
+                <td>{item.edited}</td>
+                <td>{item.url}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
